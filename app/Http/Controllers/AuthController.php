f008b4928fd87916249f0e877d74d46b12b4310e
kimeu254 <jimkimeu254@gmail.com>
2022-10-04 13:09:37 +0300

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request) 
+    {
+        $attrs = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $attrs['name'],
+            'email' => $attrs['email'],
+            'password' => bcrypt($attrs['password'])
+        ]);
+
+        return response([
+            'user' => $user,
+            'token' => $user->createToken('secret')->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request) 
+    {
+        $attrs = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        //atempt login
+        if(!Auth::attempt($attrs))
+        {
+            return response([
+                'message' => 'Invalid Credentials.'
+            ], 403);
+        }
+
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout success.'
+        ], 200);
+    }
+
+    public function user()
+    {
+        return response([
+            'user' => auth()->user()
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
